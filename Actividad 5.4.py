b7187ad27ed9f22526891a999dbf333024906958
Diegohdzl <a01734409@tec.mx>
2025-06-03 16:26:53 -0600

    Add files via upload

---
+++ Actividad 5.4.py
@@ -0,0 +1,5 @@
+# Cargar el nuevo archivo que subió el usuario
+df_upd = pd.read_csv("/mnt/data/UPDINTEGRADO_sincodificar_3categorias.xlsx - Sheet1.csv")
+
+# Mostrar las primeras filas para analizar el contenido
+df_upd.head()
